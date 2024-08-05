44ab39fc9c08418784cf52042fc3f59b08138091
niv-regev-moveo <nivr@moveo.co.il>
2024-08-05 23:58:30 +0300

    restaurants page : dispatch data in relevant places . adjusting the style of the card component to be as it should for homePage and for the restaurantsPage

--- Epicure-Front/src/Pages/RestaurantsPage/RestaurantsPage.tsx
+++ Epicure-Front/src/Pages/RestaurantsPage/RestaurantsPage.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useEffect, useMemo } from "react";
+import Card from "../../components/Common/Card";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../redux/store/store";
+import { fetchRestaurants } from "../../redux/chunk/collections/restaurants/restaurants.thunks";
+import { CardRestaurantProps } from "../../redux/chunk/collections/restaurants/restaurants.types";
+import CardRestaurant from "../../components/Sections/RestaurantsSection/CardRestaurant";
+import { StyledCardWrapper, StyledRestaurantsPageContainer } from "./styles";
 
 const RestaurantsPage = () => {
-  return <div>RestaurantsPage</div>;
+  const dispatch = useDispatch<AppDispatch>();
+  const { restaurants, loading, error } = useSelector(
+    (state: RootState) => state.restaurantsState
+  );
+
+  useEffect(() => {
+    dispatch(fetchRestaurants());
+  }, [dispatch]);
+
+  const restaurantCardsData = useMemo(() => {
+    return restaurants.map((restaurant) => ({
+      image: restaurant.image,
+      title: restaurant.name,
+      BottomComponent: CardRestaurant,
+      bottomComponentProps: {
+        chefName: restaurant.chef.name,
+        rating: restaurant.rating,
+      } as CardRestaurantProps,
+    }));
+  }, [restaurants]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return (
+    <StyledRestaurantsPageContainer>
+      {restaurantCardsData.map((cardData, index) => (
+        <StyledCardWrapper key={index}>
+          <Card
+            image={cardData.image}
+            title={cardData.title}
+            BottomComponent={cardData.BottomComponent}
+            bottomComponentProps={cardData.bottomComponentProps}
+            restaurantPageStyle={true}
+          />
+        </StyledCardWrapper>
+      ))}
+    </StyledRestaurantsPageContainer>
+  );
 };
 
 export default RestaurantsPage;
